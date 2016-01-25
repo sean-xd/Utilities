b6dbd1981eea195d66728277d508ccd28069c32c
Sean Murray <thecanbeat@gmail.com>
2016-01-25 12:47:17 -0500

    databind edit

--- dataBind.js
+++ dataBind.js
@@ -1,6 +1,19 @@
 function dataBind(element, state, property){
     element.textContent = state[property];
-    Object.defineProperty(state, property, {set: function(x){
-        element.textContent = x;
-    }});
+    Object.defineProperty(state, "_" + property, {
+        enumerable: false,
+        writable: true,
+        configurable: true,
+        value: state[property]
+    });
+    Object.defineProperty(state, property, {
+        enumerable: true,
+        set: function(x){
+            state["_" + property] = x;
+            element.textContent = x;
+        },
+        get: function(){
+            return state["_" + property];
+        }
+    });
 }
