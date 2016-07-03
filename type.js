53cec908a237fbcda3b83704901a81374ac1fac1
Sean Murray <thecanbeat@gmail.com>
2016-07-03 15:56:45 -0600

    keep unclicking this file for some reason

--- type.js
+++
@@ -1,9 +0,0 @@
-function type(obj, shell){
-    var SNA = (is(obj, "String") || is(obj, "Number") || is(obj, "Array"));
-    if(SNA) return shell ? {type: is(obj)} : {type: is(obj), value: obj};
-    var typed = {type: "Object", properties: {}};
-    Object.keys(obj).forEach(function(e){
-        typed.properties[e] = type(obj[e], shell);
-    });
-    return typed;
-}
